35c3bb7cc2533f1c2ad4240b1053ba51448f19bd
sayyidimad <sayyidimaduddin99@gmail.com>
2023-07-29 16:10:35 +0700

    login: ubah alur otentikasi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 /**
@@ -17,13 +18,36 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+        // Make an HTTP request to the API to get the user data
+        $response = Http::post('http://34.101.154.14:8175/hackathon/user/auth/create', [
+            "ktpId" => $this->faker->numerify('############'),
+            "username" => "exceeds",
+            "phoneNumber" => $this->faker->phoneNumber,
+            "loginPassword" => '12345678',
+            "birthDate" => $this->faker->date('dmY'),
+            "gender" => $this->faker->randomElement([0, 1]), // Assuming gender 1 represents male and 2 represents female
+            "email" => $this->faker->unique()->safeEmail,
+        ]);
+
+        // Check if the API request was successful
+        if ($response->successful()) {
+            $responseData = $response->json();
+            print($responseData);
+
+            // Return the received user data from the API
+            return [
+                "ktpId" => $responseData['ktpId'],
+                "username" => $responseData['username'],
+                "phoneNumber" => $responseData['phoneNumber'],
+                "password" => bcrypt('12345678'), // Assuming you want to use a fixed password for all seeded users
+                "birthDate" => $responseData['birthDate'],
+                "gender" => $responseData['gender'],
+                "email" => $responseData['email'],
+            ];
+        } else {
+            // Handle API request error
+            throw new \Exception('Failed to seed user data from API.');
+        }
     }
 
     /**
